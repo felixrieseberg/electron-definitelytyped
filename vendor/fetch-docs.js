32c21e9a2702c4758363727760503f9c414ca927
Samuel Attard <samuel.r.attard@gmail.com>
2017-03-03 22:54:43 +1100

    Whole bunch of fixes / changes

--- vendor/fetch-docs.js
+++ vendor/fetch-docs.js
@@ -7,7 +7,7 @@ const mkdirp = require('mkdirp').sync
 const os = require('os')
 
 const downloadPath = path.join(os.tmpdir(), 'electron-api-tmp')
-const ELECTRON_COMMIT = '01423f79b3fd3aa8270abd657fb83da64e39c57c'
+const ELECTRON_COMMIT = '8f7785bb05630ecf7dd7e5d4e751e69396723bb9'
 
 rm(downloadPath)
 
@@ -22,3 +22,8 @@ module.exports = target =>
   .then(() => {
     return linter(downloadPath, '9.9.9')
   })
+
+process.on('unhandledRejection', (err) => {
+  console.error(err);
+  process.exit(0);
+})
